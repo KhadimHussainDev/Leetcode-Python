a79db09a45de18f377801de1db947947156fdd11
KhadimHussainDev <oyeKhadim@gmail.com>
2024-03-30 17:31:14 +0500

    217. Contains Duplicate Solved

---
+++ 217. Contains Duplicate.py
@@ -0,0 +1,10 @@
+#Solution 1
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashset= set()
+        for num in nums:
+            if num in hashset:
+                return True
+            hashset.add(num)
+        return False
+            
