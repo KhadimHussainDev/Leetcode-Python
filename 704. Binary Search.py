d355b2cabae97c48a876c55ef17b2e59c0d04c16
KhadimHussainDev <oyeKhadim@gmail.com>
2024-04-05 12:52:29 +0500

    704. Binary Search Solved

---
+++ 704. Binary Search.py
@@ -0,0 +1,13 @@
+#Solution 1
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        start , end = 0 , len(nums) - 1 
+        while start <= end :
+            mid = start + (end - start)//2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] > target:
+                end = mid - 1
+            else:
+                start = mid + 1
+        return -1
